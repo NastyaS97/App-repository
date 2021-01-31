71e2e80988e04082836efd0f7bbd08b7010a2c85
NastyaS <nastya.soiko.97@gmail.com>
2021-01-31 03:14:17 +0300

    add solution for editting color in label

--- ColorPicker/ColorPicker/Helper.swift
+++ ColorPicker/ColorPicker/Helper.swift
@@ -7,5 +7,17 @@
 
 import UIKit
 
-extension UIColor
-
+extension UIColor {
+    
+    convenience init(red: Float, green: Float, Blue: Float, aplpha: CGFloat = 1) {
+        self.init(
+            red: CGFloat(red) / 255.0,
+            green: CGFloat(green) / 255.0,
+            blue: CGFloat(Blue) / 255.0,
+            alpha: aplpha
+        )
+    }
+    static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat = 1) -> UIColor {
+        return UIColor(red: red / 255.0, green: green / 255.0, blue: blue / 255.0, alpha: alpha)
+    }
+}
--- ColorPicker/ColorPicker/ViewController.swift
+++ ColorPicker/ColorPicker/ViewController.swift
@@ -17,9 +17,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var secondElementForInput: UILabel! // надпись Грин
     @IBOutlet weak var thirdElementForInput: UILabel! // надпись Блю
     
-    @IBOutlet weak var fildForFirstInputting: UITextField! { // первая строка ввода
+   
+    @IBOutlet weak var firstForFirstInputting: UITextField! {
         didSet {
-            self.fildForFirstInputting.clearButtonMode = .always // удаляет текст одной кнопкой если он написан
+            self.firstForFirstInputting.clearButtonMode = .always // удаляет текст одной кнопкой если он написан
         }
     }
     @IBOutlet weak var secondForFirstInputting: UITextField! {
@@ -32,55 +33,70 @@ class ViewController: UIViewController {
             self.fildForThirdtInputting.clearButtonMode = .always // удаляет текст одной кнопкой если он написан
         }
     }
-
+    
     @IBOutlet weak var messageAboutResult: UILabel! // изменяемая часть где выводит предупреждение
-    @IBOutlet weak var viewOfChanchingColor: UIView! { // квадрат изменяет цвет
-        didSet {
-            self.viewOfChanchingColor.backgroundColor = .white
-        }
-    }
+    @IBOutlet weak var viewOfChanchingColor: UIView!  // квадрат изменяет цвет
+//            didSet {
+//                self.viewOfChanchingColor.backgroundColor
+//            }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+  
     @IBAction func firstField(_ sender: UITextField) {
         sender.resignFirstResponder() // ретерн
     }
+    
     @IBAction func secondField(_ sender: UITextField) {
         sender.resignFirstResponder() // ретерн
     }
+    
     @IBAction func thirdField(_ sender: UITextField) {
-        sender.resignFirstResponder() // ретерн
+        sender.resignFirstResponder()
+    } // ретерн
+    
+    
+    @IBAction func buttonTouched(_ sender: Any) {
+        
+        let redIn: Float  = Float(self.firstForFirstInputting!.text ?? "0") ?? 0.0
+        let greenIn: Float = Float(self.secondForFirstInputting!.text ?? "0") ?? 0.0
+        let blueIn: Float = Float(self.fildForThirdtInputting!.text ?? "0") ?? 0.0
+        
+        viewOfChanchingColor.backgroundColor = UIColor(red: redIn, green: greenIn, Blue: blueIn, aplpha: 1)
     }
     
-//    @IBAction func buttonTouched(_ sender: UIButton) {
-//        if let inputText = self.fildForFirstInputting.text, !inputText.isEmpty {
-//            self.messageAboutResult(with: "Invalid color parameters! Please, check them.")
-//            self.viewOfChanchingColor.backgroundColor = .white
-//        } if else let inputText?.characters.count < 0 {
-//            self.changeAppDescription(with: "ERROR")
-//        }
-//    }
+    //        if let inputText = self.fildForFirstInputting.text, !inputText.isEmpty {
+    //            self.messageAboutResult(with: "Invalid color parameters! Please, check them.")
+    //            self.viewOfChanchingColor.backgroundColor = .white
+    //        } if else let inputText?.characters.count < 0 {
+    //            self.changeAppDescription(with: "ERROR")
+    //        }
+    //    }
     
-    func setUpView() {
-        self.logoLabel.textColor = .orange
-
-        self.setButton(text: "Print!")
-
-        self.changeAppDescription(with: "ERROR")
-    }
-
-//func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
-//    let isNumber = NSCharacterSet.decimalDigitCharacterSet().isSupersetOfSet(NSCharacterSet(charactersInString: string))
-//
-//    return isNumber || (string == NSNumberFormatter().decimalSeparator && textField.text?.containsString(string) == false)
-//}
-
-    func setButton(text: String) {
-        self.mainButtonofApp.setTitle(text, for: .normal)
-    }
-
-    func changeAppDescription(with text: String) {
-        self.messageAboutResult.text = text // функция изменения текста
-    }
+    //    func setUpView() {
+    //        self.logoLabel.textColor = .orange
+    //
+    //        self.setButton(text: "Print!")
+    //
+    //        self.changeAppDescription(with: "ERROR")
+    //    }
+    
+    //func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+    //    let isNumber = NSCharacterSet.decimalDigitCharacterSet().isSupersetOfSet(NSCharacterSet(charactersInString: string))
+    //
+    //    return isNumber || (string == NSNumberFormatter().decimalSeparator && textField.text?.containsString(string) == false)
+    //}
+    
+    //    func setButton(text: String) {
+    //        self.mainButtonofApp.setTitle(text, for: .normal)
+    //    }
+    //
+    //    func changeAppDescription(with text: String) {
+    //        self.messageAboutResult.text = text // функция изменения текста
+    //    }
+    //}
+    
 }
-
