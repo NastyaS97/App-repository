5b0fcee9690aaab53b8972eac7a6cfb3a5bbcfde
NastyaS <nastya.soiko.97@gmail.com>
2021-01-31 18:29:36 +0300

    add all solution

--- ColorPicker/ColorPicker/ViewController.swift
+++ ColorPicker/ColorPicker/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var secondElementForInput: UILabel! // надпись Грин
     @IBOutlet weak var thirdElementForInput: UILabel! // надпись Блю
     
-   
+    
     @IBOutlet weak var firstForFirstInputting: UITextField! {
         didSet {
             self.firstForFirstInputting.clearButtonMode = .always // удаляет текст одной кнопкой если он написан
@@ -35,17 +35,17 @@ class ViewController: UIViewController {
     }
     
     @IBOutlet weak var messageAboutResult: UILabel! // изменяемая часть где выводит предупреждение
-    @IBOutlet weak var viewOfChanchingColor: UIView!  // квадрат изменяет цвет
-//            didSet {
-//                self.viewOfChanchingColor.backgroundColor
-//            }
-    
+    @IBOutlet weak var viewOfChanchingColor: UIView! {  // квадрат изменяет цвет
+        didSet {
+            self.viewOfChanchingColor.backgroundColor = .white
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-  
+    
     @IBAction func firstField(_ sender: UITextField) {
         sender.resignFirstResponder() // ретерн
     }
@@ -60,43 +60,39 @@ class ViewController: UIViewController {
     
     
     @IBAction func buttonTouched(_ sender: Any) {
+        if let text = self.firstForFirstInputting.text, text.isEmpty {
+            self.changeAppDescription(text: "Invalid color parameters! Please, check them.")
+        } else {
+            if let text = self.secondForFirstInputting.text, text.isEmpty {
+                self.changeAppDescription(text: "Invalid color parameters! Please, check them.")
+            } else {
+                if let text = self.fildForThirdtInputting.text, text.isEmpty {
+                    self.changeAppDescription(text: "Invalid color parameters! Please, check them.")
+                } else {
+                    self.changeAppDescription(text: "You can try again!")
+                }
+            }
+        }
         
         let redIn: Float  = Float(self.firstForFirstInputting!.text ?? "0") ?? 0.0
         let greenIn: Float = Float(self.secondForFirstInputting!.text ?? "0") ?? 0.0
         let blueIn: Float = Float(self.fildForThirdtInputting!.text ?? "0") ?? 0.0
         
-        viewOfChanchingColor.backgroundColor = UIColor(red: redIn, green: greenIn, Blue: blueIn, aplpha: 1)
+        viewOfChanchingColor.backgroundColor = UIColor(red: redIn, green: greenIn, Blue: blueIn, aplpha: 1.0)
     }
     
-    //        if let inputText = self.fildForFirstInputting.text, !inputText.isEmpty {
-    //            self.messageAboutResult(with: "Invalid color parameters! Please, check them.")
-    //            self.viewOfChanchingColor.backgroundColor = .white
-    //        } if else let inputText?.characters.count < 0 {
-    //            self.changeAppDescription(with: "ERROR")
-    //        }
-    //    }
-    
-    //    func setUpView() {
-    //        self.logoLabel.textColor = .orange
-    //
-    //        self.setButton(text: "Print!")
-    //
-    //        self.changeAppDescription(with: "ERROR")
-    //    }
-    
-    //func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
-    //    let isNumber = NSCharacterSet.decimalDigitCharacterSet().isSupersetOfSet(NSCharacterSet(charactersInString: string))
-    //
-    //    return isNumber || (string == NSNumberFormatter().decimalSeparator && textField.text?.containsString(string) == false)
-    //}
-    
-    //    func setButton(text: String) {
-    //        self.mainButtonofApp.setTitle(text, for: .normal)
-    //    }
-    //
-    //    func changeAppDescription(with text: String) {
-    //        self.messageAboutResult.text = text // функция изменения текста
-    //    }
-    //}
-    
+    func setUpView() {
+        self.logoLabel.textColor = .orange
+        
+        self.setButton(text: "Print!")
+        
+        self.changeAppDescription(text: "You can start!")
+    }
+    func setButton(text: String) {
+        self.mainButtonofApp.setTitle(text, for: .normal)
+    }
+    func changeAppDescription(text: String) {
+        self.messageAboutResult.text = text // функция изменения текста
+    }
 }
+
