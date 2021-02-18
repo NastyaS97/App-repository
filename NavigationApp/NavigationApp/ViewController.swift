addd1a7e504e27819fac0c188b2dad75f67ab6d5
NastyaS <nastya.soiko.97@gmail.com>
2021-02-18 20:11:47 +0300

    added textView

--- NavigationApp/NavigationApp/ViewController.swift
+++ NavigationApp/NavigationApp/ViewController.swift
@@ -8,15 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+    //MARK: - outlets
     @IBOutlet weak var maneView: UIView!
     
-
+    @IBOutlet weak var nameView: TitleDescriptionView!
+    @IBOutlet weak var locationView: TitleDescriptionView!
+    @IBOutlet weak var occupetionView: TitleDescriptionView!
+    @IBOutlet weak var editinfoButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func shareButtonTapped(_ sender: Any) {
+        let text = "Hellow, i'm app"
+        guard let url = URL(string: "google.com") else { return }
+        let activityController = UIActivityViewController(activityItems: [text, url], applicationActivities: nil)
+        
+        self.present(activityController, animated: true) {
+            print("I was presented")
+        }
+    }
 }
 
