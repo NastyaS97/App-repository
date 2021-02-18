caa7a3a35a6c40db19a4ace403ac5c9f355ddb4f
NastyaS <nastya.soiko.97@gmail.com>
2021-02-18 21:14:25 +0300

    added mode with buttonPushed

--- NavigationApp/NavigationApp/EditinfoController.swift
+++ NavigationApp/NavigationApp/EditinfoController.swift
@@ -9,13 +9,44 @@ import UIKit
 
 class EditinfoController: UIViewController {
 
+    @IBOutlet weak var saveButton: UIButton!
+    @IBOutlet weak var infoTextFiel: UITextField!
+    @IBOutlet weak var textField: UITextView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        let swipeRecognizer = UISwipeGestureRecognizer(target: self,
+                                                       action: #selector(self.viewWasSwiped))
+        swipeRecognizer.direction = .down
+        self.view.addGestureRecognizer(swipeRecognizer)
+        
+        // 1 way
+//        let recognizer = UITapGestureRecognizer(target: self,
+//                                                action: #selector(self.saveButtonTapped))
+//        saveButton.addGestureRecognizer(recognizer)
+        
+        // 2 way
+//        let saveButtonAction = UIAction { (_) in
+//            print("saveButtomTapped")
+//        }
+//        self.saveButton.addAction(saveButtonAction, for: .touchUpInside)
+        
+        
+        // 3 way
+        self.saveButton.addTarget(self, action: #selector(self.saveButtonTapped),
+                                  for: .touchUpInside)
     }
     
+    @objc func saveButtonTapped() {
+        self.dismiss(animated: true, completion: nil) // if cotroller was presented
+//        self.navigationController?.popViewController(animated: true) //  возврат к предыдущей странице после нажатия кнопки if cotroller was pushed
+    }
 
+    @objc func viewWasSwiped() {
+        print("swipe")
+    }
+    
     /*
     // MARK: - Navigation
 
